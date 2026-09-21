f08a03825672b5dd75ee38de548f17d1c7074c3a
Wilman Alzate <wilmanalzate@yahoo.com>
2026-09-20 20:28:03 -0500

    Implementa clasificador de textos por ODS en Streamlit

--- IMG_Classifier/src/DataPreprocessing.py
+++ IMG_Classifier/src/DataPreprocessing.py
@@ -1,27 +1,47 @@
-import numpy as np
-import pandas as pd
+import re
+import spacy
 
 class DataPreprocessing:
+    """
+    Clase encargada del preprocesamiento de los textos.
+    Reproduce el mismo procedimiento utilizado durante el entrenamiento del modelo del Microproyecto 2.
+    """
 
     def __init__(self):
         print("DataPreprocessing.__init__ ->")
-        self.MAX_PIXEL_COUNT = 784
+        # Se carga el mismo modelo de spaCy utilizado durante el desarrollo y entrenamiento.
+        self.nlp = spacy.load("es_core_news_sm")
 
-    def transform(self, df):
+    def transform(self, texto):
+        """
+        Limpia y lematiza un texto en español para preparar su análisis.
+
+        Parámetro:
+            texto (str): Texto original de un documento.
+
+        Retorna:
+            str: Lemas separados por espacios, después de limpiar el texto y excluir tokens no alfabéticos y palabras vacías.
+        """
+
         print("DataPreprocessing.transform ->")
-        return None
 
-    def get_columns(self):
-        print("DataPreprocessing.get_columns ->")
-        #TO-DO: Genera los nombres de las columnas en una lista para la variable res
-        res = []
+        # Unifica mayúsculas y minúsculas para evitar diferencias de escritura en la representación del vocabulario.
+        texto = texto.lower()
+        # Eliminar URLs.
+        texto = re.sub(r'http\S+|www\S+', ' ', texto)
+        # Eliminar correos electrónicos.
+        texto = re.sub(r'\S+@\S+', ' ', texto)
+        # Reemplaza guiones y rayas por espacios.
+        texto = re.sub(r"[-‐-]", " ", texto)
+        # Procesa el texto con spaCy.
+        doc = self.nlp(texto)
 
-        return set(res)
+        # Conserva tokens alfabéticos, elimina palabras vacías y utiliza la forma lematizada.
+        # También se verifica que el lema obtenido no sea una stopword.
+        tokens = [token.lemma_ for token in doc if token.is_alpha and not token.is_stop
+            and token.lemma_ not in self.nlp.Defaults.stop_words]
 
-    def get_cat_name(self, index):
-        print("DataPreprocessing.get_cat_name ->")
-        if index < 0 or index > 1:
-            return ""
-        return self.get_categories()[index]
+        # Une los lemas para que el resultado pueda ser utilizado posteriormente por TF-IDF.
+        return ' '.join(tokens)
     
 
--- IMG_Classifier/src/ModelController.py
+++ IMG_Classifier/src/ModelController.py
@@ -1,63 +1,70 @@
 import Definitions
 
-import numpy as np
 import os.path as osp
 import pandas as pd
-from io import StringIO
+import joblib
 
-#TO-DO. Importar la libreria joblib
-from src.DataPreprocessing import DataPreprocessing
 
 class ModelController:
 
     def __init__(self):
         print("ModelController.__init__ ->")
-        # Asegura en una variable la ruta de los modelos
-        self.model_path = osp.join(Definitions.ROOT_DIR, "resources/models")
-        # Almacena la ruta de cada modelo en una variable        
-        self.pca_path = osp.join(self.model_path, "pca.joblib")
-        self.scaler_path = osp.join(self.model_path, "scaler.joblib")
-        self.model_path = osp.join(self.model_path, "model.joblib")
 
-        #TO-DO: Cargar los modelos
-        self.pca = None
-        self.scaler = None
-        self.model = None
+        # Define la carpeta donde se encuentran los componentes entrenados que utilizará la aplicación.
+        self.models_path = osp.join(Definitions.ROOT_DIR, "resources/models")
 
-        # Inicializar variables
-        self.input_df = ""
-        # Clase de preprocesamiento de la información
-        self.d_processing = DataPreprocessing()
+        # Construye la ruta del vectorizador TF-IDF entrenado.
+        self.vectorizer_path = osp.join(self.models_path, "vectorizador_tfidf.joblib")
 
-    def validate_data(self, df):
-        #Compara los nombres de las columnas con el archivo
-        return self.d_processing.get_columns().issubset(set(df.columns))
+        # Construye la ruta del modelo SVD utilizado para reducir la dimensionalidad de la representación TF-IDF.
+        self.svd_path = osp.join(self.models_path,"svd.joblib")
+
+        # Construye la ruta del modelo final de clasificación: regresión logística.
+        self.model_path = osp.join(self.models_path, "modelo.joblib")
+
+        # Carga los tres componentes entrenados.
+        self.vectorizer = joblib.load(self.vectorizer_path)
+        self.svd = joblib.load(self.svd_path)
+        self.model = joblib.load(self.model_path)
+
     
-    def get_categories(self):
-        print("ModelController.get_categories ->")
-        return ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']    
+    def predict(self, texto):
+        """
+        Clasifica un texto utilizando los componentes entrenados en el Microproyecto 2.
 
-    def load_input_data(self, input_data):
-        print("ModelController.load_input_data ->")
-        try:
-            input_data_str = StringIO(input_data.getvalue().decode("utf-8"))
-            self.input_df = pd.read_csv(input_data_str)
-            is_valid = self.validate_data(self.input_df)
-            return self.input_df, is_valid
+        Parámetro:
+            texto (str): Texto libre ingresado por el usuario.
 
-        except:
-            raise("Ocurrió un error al leer la información de entrada")
+        Retorna:
+            ods_predicho: número del ODS predicho.
+            probabilidad_predicha: probabilidad estimada para el ODS predicho.
+            probabilidades: DataFrame con las probabilidades de todos los ODS.
+        """
 
-    def predict(self, data):
         print("ModelController.predict ->")
-        X = data[1:].to_numpy()
-        Y = data.iloc[0]
-        #TO-DO: Escala los datos
-        X_scaled = None
-        #TO-DO: Reduce los datos
-        X_reduced = None
-        #TO-DO: Genera la predicción
-        y_pred = None
-        
-        return X, Y, y_pred
+
+        # Validamos que se haya recibido un texto.
+        if not isinstance(texto, str) or not texto.strip():
+            raise ValueError("Debe ingresar un texto para realizar la predicción.")
+
+        # El vectorizador aplica el mismo preprocesamiento utilizado
+        # durante el entrenamiento y genera la representación TF-IDF.
+        X_tfidf = self.vectorizer.transform([texto])
+
+        # Aplicamos la reducción de dimensionalidad SVD entrenada.
+        X_reduced = self.svd.transform(X_tfidf)
+
+        # Obtenemos el ODS predicho.
+        ods_predicho = self.model.predict(X_reduced)[0]
+
+        # Calculamos la probabilidad estimada para cada ODS.
+        probabilidades_modelo = self.model.predict_proba(X_reduced)[0]
+
+        # Relacionamos cada probabilidad con la clase correspondiente.
+        probabilidades = pd.DataFrame({"ODS": self.model.classes_, "Probabilidad": probabilidades_modelo})
+
+        # Recuperamos la probabilidad correspondiente al ODS predicho.
+        probabilidad_predicha = probabilidades.loc[probabilidades["ODS"] == ods_predicho, "Probabilidad"].iloc[0]
+
+        return ods_predicho, probabilidad_predicha, probabilidades
 
